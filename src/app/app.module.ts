4f161b27f764e1930ba89276178d94857db64fe6
[lorenagiraldo1031@gmail.com] <[lorenagiraldo1031@gmail.com]>
2017-09-10 10:52:46 -0500

    librerias ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './customers/index/index.component';
+import { AddComponent  as OrderAddComponent }  from './orders/add/add.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import {CustomersModule} from './customers/customers.module';
+import { CustomersModule } from './customers/customers.module';
+import { MenuComponent } from './menu/menu.component';
+import { OrdersModule } from './orders/orders.module';
+import { CurrentOptionComponent } from './menu/current-option/current-option.component';
 
+
+
+const appRoutes:Routes =[
+  {path: 'customers/index', component: IndexComponent},
+  {path: 'orders/add',      component: OrderAddComponent},
+];
 
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [    
+    AppComponent,    
+    MenuComponent,
+    CurrentOptionComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomersModule,
+    OrdersModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddComponent } from './add/add.component';
+import { IndexComponent } from './index/index.component';
+import { ComponentComponent } from './component/component.component';
+import { CustomersComponent } from './customers.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AddComponent]
+  declarations: [AddComponent, IndexComponent, ComponentComponent, CustomersComponent]
 })
 export class CustomersModule { }
